4d0ba25e9f66b5cf4377aa7d679130712c4510ef
roshangrewal <roshangrewal@hotmail.com>
2020-06-15 21:02:33 +0530

    mongodb atlas configuration done

---
+++ models/Bootcamp.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const BootcampSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please add a name'],
+    unique: true,
+    trim: true,
+    maxlength: [50, 'Name can not be more than 50 characters'],
+  },
+  // url friendly version of the name. eg. if we have name like Devcentral Bootcamp then it will slug would be in all lowercase devcentral-bootcamp
+  slug: String,
+  description: {
+    type: String,
+    required: [true, 'Please add a description'],
+    maxlength: [500, 'Description can not be more than 500 characters'],
+  },
+});
--- server.js
+++ server.js
@@ -1,14 +1,44 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const colors = require('colors');
+const logger = require('./middleware/logger');
+const connectDB = require('./config/db');
 
 // Load env variables
 dotenv.config({ path: './config/config.env' });
 
+// Connect to database
+connectDB();
+
+// Route files
+const bootcamps = require('./routes/bootcamps');
+
 // Initialize express application
 const app = express();
 
+// Custom middleware for logging request method, protocol, host, and url
+// app.use(logger);
+
+// Dev logging middleware
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
+// Mount routers
+app.use('/api/v1/bootcamps', bootcamps);
+
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server runnung in ${process.env.NODE_ENV} mode on port ${PORT}`);
+const server = app.listen(PORT, () => {
+  console.log(
+    `Server runnung in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  );
+});
+
+//  Handle unhandled promise rejections
+process.on('unhandledRejection', (err, promise) => {
+  console.log(`Error: ${err.message}`.red);
+  // Close server & exit process
+  server.close(() => process.exit(1));
 });
